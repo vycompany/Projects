b70cac7857694fded18ca63b5fb93aa4ae43bb26
vycompany <yaja14cs@cmrit.ac.in>
2017-06-03 00:10:57 +0530

    Add files via upload

---
+++ dreamgirl/contact.php
@@ -0,0 +1,191 @@
+<!-- PHP .................. -->
+<html>
+	<head>
+<style>
+footer {
+    padding: 0em;
+    color: white;
+    background-color: black;
+    clear: left;
+    position:relative;
+    bottom:-100;
+    text-align: center;
+}
+</style>
+		<title>Empowered Women</title>		
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+			<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<!--link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'-->
+		<link rel="stylesheet" href="css/responsiveslides.css">
+		<script src="js/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }>
+</script>
+
+
+	</head>
+<body>
+<!-----start-header----->
+<!-----start-header----->
+<div class="header">
+	 <!---start-container---->
+	 <div class="container">
+				<!---start-top-header---->
+				<div class="top-header">
+					<div class="top-header-left">
+						<p style="font-size:80px;">DREAM GIRL</p>
+					</div>
+					<div class="top-header-right">
+						<div class="top-header-contact-info">
+							
+						</div>
+						<div class="top-header-contact-account">
+							<div class="social">
+								<ul>
+									<li><a href="dummy.php"><i class="facebook"></i></a></li>
+									<li><a href="dummy.php"><i class="twitter"></i></a></li>
+									<li><a href="dummy.php"><i class="dribble"></i></a></li>	
+									<li><a href="dummy.php"><i class="google"></i></a></li>										
+								</ul>
+							</div>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<!---End-top-header---->
+				<!----start-main-header----->
+				<div class="main-header">
+					<div class="logo">
+						<a href="index.html"><title="" /></a>
+					</div>
+					<div class="top-nav">
+					<span class="menu"></span>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="services.php">Services</a></li>
+							<li><a href="blog.php">Blog</a></li>
+							<li class="active"><a href="contact.php">Contact</a></li>
+							<li><a href="admin.php">admin login</a></li>
+							<div class="clearfix"> </div>
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+					</div>
+					<div class="grid_12">
+					  <img src="images/clr.jpg" class="img-responsive">
+					</div>
+			  <!-- script-for-menu -->
+		 <script>
+				$("span.menu").click(function(){
+					$(".top-nav ul").slideToggle("slow" , function(){
+					});
+				});
+		 </script>
+		 <!-- script-for-menu -->	 
+			
+					
+	 </div>
+	<!----End-main-header----->
+</div>
+		<!---start-content----->
+		<div class="content">
+			<div class="container">
+				<!---start-contact---->
+				<div class="contact">
+					<div class="contact-header">
+							<h3>Contact</h3>
+							<ol class="breadcrumb">
+							  <li><a href="dummy.php">Home</a></li>
+							  <li class="active">Contact</li>						  
+							</ol>
+				  </div>
+				  <div class="contact_head">
+				   <p>For your feedback or any queries, please contact us: </p>
+			 </div>	
+		 <div class="contact-grids">	
+			  <div class="col-md-6 contact_left">
+			 		<h3>Contact Form</h3>
+				  <form id="form" name="form" method="post" action=" ">
+					 <div class="form_details">
+						   <input type="text" class="text" value="Name" name="Name" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Name';}">
+						   <input type="text" class="text" value="Email Address" name="Email" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Email Address';}">
+							<input type="text" class="text" value="Subject" name="Subject" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Subject';}">
+							<textarea value="Message" name="Message" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Message';}">Message</textarea>
+							<div class="clearfix"> </div>					   
+							<input name="submit" type="submit" value="Send message">
+					 </div>					 
+				  </form>
+			 </div>
+			 <div class="col-md-6 contact_right">	
+				   <h3>Address</h3>
+				  <div class="address">
+					 <ul>
+					 <li>CMRIT,dummy.php136,AECS Layout,</li>
+					 <li>Kundahalli,ITPL Road,Bangalore</li>
+					 <li>Phone:(11) 222 666 444</li>
+					 <li>Fax: (1212) 123 45 678</li>
+					 <li>Email:   <a href="mailto:info@example.com">info(at)example.com</a></li>
+					 <li>Follow on:  <a href="www.facebook.com">Facebook</a><a href="www.twitter.com">Twitter</a></li>
+					 </ul>
+				  </div>					
+			 </div>
+			 <div class="clearfix"></div>
+		 </div>
+
+				  <div class="cmrmap"><img src="cmrmap.png" width="1073" height="466"onclick="window.open('http://www.yourmapmaker.com/preview.php?a=cmrit college aecs layout bangalore,India&w=1370&h=650&n=cmrit&z=14&t=Map')">"</div>
+			<!---End-contact---->		
+			</div>
+		</div>
+		<div class="footer">
+			 <div class="container">				 	
+				 <div class="col-md-3 ftr_navi ftr">
+					 <h3>NAVIGATION</h3>
+					 <ul>
+						<li><a href="index.html">Home</a></li>
+						 <li><a href="about.html">About</a></li>
+						 <li><a href="services.php">Services</a></li>						
+						 <li><a href="blog.php">Blog</a></li>
+						 <li><a href="contact.php">Contact</a></li>
+						 
+					 </ul>
+				 </div>
+				 <div class="col-md-3 ftr_navi ftr">
+					 <h3>MEMBERS</h3>
+					 <ul>
+						 <li><a href="dummy.php">Customer Support</a></li>
+						 <li><a href="dummy.php">Platinum Support</a></li>
+						 <li><a href="dummy.php">Gold Support</a></li>						
+						 <li><a href="dummy.php">Standard Support</a></li>
+						 <li><a href="dummy.php">Training</a></li>
+					 </ul>
+				 </div>
+				 <div class="col-md-3 get_in_touch ftr">
+					  <h3>GET IN TOUCH</h3>
+					  <p>Ola-ola street,</p>
+					  <p>260-14 City, Country</p>
+					  <p>+62 000-0000-00</p>
+					  <p>email[at]example-site.com</p>
+					  <a href="mailto:mail@mlampah.com">www.example.com</a>
+				 </div>
+				 <div class="col-md-3 ftr-logo">
+					 <a href="index.html"><img src="images/572314-women-empowerment-talk-empower.jpg" alt=""/></a>
+					 <p>&copy; 2015 All Rights Reseverd Design by CMRIT Dream Girl Team  </p>
+				 </div>
+				<div class="clearfix"></div>
+			 </div>
+		  </div>
+	<!---End-container---->
+	<footer> <h1>Copyright © HVY GROUP</h1></footer>
+
+	</body>
+</html>
+
